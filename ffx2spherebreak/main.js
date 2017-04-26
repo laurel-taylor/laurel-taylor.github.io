785920e0564dc77fa3f75512958f6ab0a13826c8
Laurel Bruggeman <laurel.bruggeman@infusionsoft.com>
2017-04-26 10:28:00 -0700

    clean up sphere break code

--- ffx2spherebreak/main.js
+++ ffx2spherebreak/main.js
@@ -1,74 +1,70 @@
-$(document).ready(function() {
+function getCombos(echo, core)
+{
+  const gold = [2, 3, 5, 6];
+  const silver = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+  let results = new Array();
+
+  for (let i = 0; i < gold.length; i++) {
+    if (gold[i] % core !== 0) {
+      if (echo === 2) {
+        for (let j = 0; j < silver.length; j++) {
+          const x = gold[i] + silver[j];
+
+          if (x % core === 0) {
+            results.push([gold[i], silver[j]]);
+          }
+        }
+      } else if(echo === 3) {
+        for (let j = 0; j < silver.length; j++) {
+          const y = gold[i] + silver[j];
+
+          if (y % core !== 0) {
+            for (let k = 0; k < silver.length; k++) {
+              const x = gold[i] + silver[j] + silver[k];
+
+              if (x % core === 0) {
+                results.push([gold[i], silver[j], silver[k]]);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  const newline = "<br/>"
+  let string = newline;
+  for (let i=0; i<results.length; i++) {
+    if(echo === 2) {
+      string += "<span class='entry'>" + results[i][0] + "</span> + " + results[i][1] + newline;
+    }
+    else if (echo === 3){
+      string += "<span class='entry'>" + results[i][0] + "</span> + " + results[i][1] + " + " + results[i][2] + newline;
+    }
+  }
+
+  document.getElementById('combos').innerHTML = string;
+}
+
+$(document).ready( function() {
   $('#go').click( function() {
-     myFunction();
+    getCombos(parseInt($('#echo').val()), parseInt($('#core').val()));
   });
 
   $(document).on('keyup change', '#core, #echo', function(){
-      var $this = $(this);
-      myFunction($this.attr('value'));
-      $('#core').select();
+    var $this = $(this);
+    getCombos(parseInt($('#echo').val()), parseInt($('#core').val()));
+    $('#core').select();
   })
   .on('focus', '#core', function(){
-      var $this = $(this);
-      $this.select();
+    var $this = $(this);
+    $this.select();
 
-      // Work around Chrome's little problem
-      $this.mouseup(function() {
-          // Prevent further mouseup intervention
-          $this.unbind("mouseup");
-          return false;
-      });
+    // Work around Chrome's little problem
+    $this.mouseup(function() {
+      // Prevent further mouseup intervention
+      $this.unbind("mouseup");
+      return false;
+    });
   });
-  function myFunction()
-  {
-
-      var c = document.getElementById('core').value;
-      var g = [2, 3, 5, 6];
-      var s = [9,8,7,6,5,4,3,2,1];
-      var results = new Array();
-      var selectElt = document.getElementById('echo');
-      var echo = selectElt.options[selectElt.selectedIndex].value;
-
-      for(var i=0; i<g.length; i++){
-        if(g[i]%c!=0){
-           if(echo==2){
-            for(var j=0; j<s.length; j++){
-              var x = g[i] + s[j];
-              if(x%c==0){
-                 results.push([g[i], s[j]]);
-              }
-            }
-           }
-           else if(echo==3){
-             for(var j=0; j<s.length; j++){
-               var y = g[i] + s[j];
-               if(y%c!=0){
-                  for(var k=0; k<s.length; k++){
-                    var x = g[i] + s[j] + s[k];
-                    if(x%c==0){
-                       results.push([g[i], s[j], s[k]]);
-                    }
-                  }
-               }
-             }
-           }
-
-        }
-
-      }
-
-      var newline = "<br/>"
-      var string=newline;
-      for(var i=0; i<results.length; i++){
-          if(echo==2){
-             string+="<span class='entry'>" + results[i][0] + "</span> + " + results[i][1] + newline;
-          }
-          else if (echo==3){
-             string+="<span class='entry'>" + results[i][0] + "</span> + " + results[i][1] + " + " + results[i][2] + newline;
-          }
-
-      }
-
-      document.getElementById('combos').innerHTML = string;
-  }
 })
