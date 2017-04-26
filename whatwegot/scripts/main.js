069dadad67aaf2d9fd8031c442d5240aa9775990
Laurel Bruggeman <laurel.bruggeman@infusionsoft.com>
2017-04-26 01:42:47 -0700

    adding rockband update utility

--- whatwegot/scripts/main.js
+++ whatwegot/scripts/main.js
@@ -1,75 +1,197 @@
 var index;
 
-function goTo(a, index) {
-  setDisplay(a, index)
-  return index
+function updateList(list, index, result, comeBack) {
+  list[index] = [list[index][0], list[index][1], result, comeBack];
+  save(list, index)
+  return list;
 }
 
-function updateList(a, index, result, comeBack) {
-  a[index] = [a[index][0], a[index][1], result, comeBack];
-  return a;
+function updateIndex(index) {
+  saveIndex(index);
+  return index;
 }
 
-function setDisplay(a, index) {
-  $('#title').html(a[index][1])
-  $('#artist').html(a[index][0])
+function goTo(list, index) {
+  $('#titleUp2').html(list[index-2] ? list[index-2][1] : '')
+  $('#artistUp2').html(list[index-2] ? list[index-2][0] : '')
+  $('#titleUp1').html(list[index-1] ? list[index-1][1] : '')
+  $('#artistUp1').html(list[index-1] ? list[index-1][0] : '')
+  $('#title').html(list[index][1])
+  $('#artist').html(list[index][0])
+  $('#titleDown1').html(list[index+1] ? list[index+1][1] : '')
+  $('#artistDown1').html(list[index+1] ? list[index+1][0] : '')
+  $('#titleDown2').html(list[index+2] ? list[index+2][1] : '')
+  $('#artistDown2').html(list[index+2] ? list[index+2][0] : '')
   var $s = $('#songList')
 
   $('.highlighted').removeClass('highlighted')
-  if (a[index][2].toLowerCase().indexOf('y') === 0) {
+  if (list[index][2].toLowerCase().indexOf('y') === 0) {
     $('#yes').addClass('highlighted')
   } else {
     $('#no').addClass('highlighted')
   }
+
+  if (list[index][3]) {
+    $('#star').addClass('highlighted')
+  }
+}
+
+function convertToJS (list) {
+  var start = 'function buildSongList() { \nvar a = new Array;';
+  var end = '\nreturn a; \n}';
+  var middle = '';
+  for(var i=0; i<list.length; i++) {
+    var s = '\na.push([';
+    var row = list[i];
+    for(var j=0; j<4; j++) {
+      s += '"' + (row[j] || '') + '"' + (j===3 ? '' : ', ');
+    }
+    s += ']);'
+    middle += s;
+  }
+  return start + middle + end;
+}
+
+function stripArticles (str) {
+  if(str.indexOf('the ') === 0) {
+    return str.substr(4)
+  }
+  if(str.indexOf('a ') === 0) {
+    return str.substr(2)
+  }
+  if(str.indexOf('an ') === 0) {
+    return str.substr(3)
+  }
+  return str;
+}
+
+function sortList (list) {
+  return list.sort(function(first, second) {
+    var aA = stripArticles(first[0].toLowerCase())
+    var aB = stripArticles(second[0].toLowerCase())
+    if (aA === aB) {
+      var x = stripArticles(first[1].toLowerCase())
+      var y = stripArticles(second[1].toLowerCase())
+      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    }
+    return ((aA < aB) ? -1 : ((aA > aB) ? 1 : 0));
+  })
 }
 
 function init () {
-  index = 0;
-  var a = buildSongList();
-  setDisplay(a, index);
+  let songList;
+  if (localStorage.getItem('rockband_list')) {
+    songList = JSON.parse(localStorage.getItem('rockband_list'))
+    index = parseInt(localStorage.getItem('rockband_index'))
+    console.log('has list, index:', index)
+  } else {
+    songList = buildSongList();
+    songList = sortList(songList);
+    index = 0;
+  }
+  goTo(songList, index)
   $('#results').val('');
-  return a;
+  return songList;
 }
+
+function save(list, index) {
+  console.log('save called', index)
+  localStorage.setItem('rockband_list', JSON.stringify(list))
+  localStorage.setItem('rockband_index', index)
+}
+function saveIndex(index) {
+  console.log('save index called', index)
+  localStorage.setItem('rockband_index', index)
+}
+
 $(document).ready(function(){
-  var a = init();
+  let a = init();
 
   $('#yes').on('click', function(e) {
     a = updateList(a, index, 'y', a[index][3] || '')
-    index = goTo(a, index + 1)
+    if( index < a.length ) {
+      index = updateIndex(index + 1)
+      goTo(a, index)
+    }
   })
 
   $('#no').on('click', function(e) {
     a = updateList(a, index, '', a[index][3] || '')
-    index = goTo(a, index + 1)
+    if( index < a.length ) {
+      index = updateIndex(index + 1)
+      goTo(a, index)
+    }
   })
 
   $('#star').on('click', function(e) {
-    a = updateList(a, index, a[index][2], 'come back')
+    if (a[index][3]) {
+      a = updateList(a, index, a[index][2], '')
+    } else {
+      a = updateList(a, index, a[index][2], 'come back')
+    }
+    goTo(a, index)
   })
 
-  $('#results').click(function(){
-    $(this).select();
-  });
+  $('#up').on('click', function(e) {
+    if( index > 0 ) {
+      index = updateIndex(index - 1)
+      goTo(a, index)
+    }
+  })
+
+  $('#down').on('click', function(e) {
+    if( index < a.length ) {
+      index = updateIndex(index + 1)
+      goTo(a, index)
+    }
+  })
+
+  $('#convert').on('click', function(e) {
+    $('#results').val(convertToJS(a));
+  })
+
+  $('#save').on('click', function(e) {
+    save(a, index);
+    $('#results').val(convertToJS(a));
+  })
+
+  $('#explode').on('click', function(e) {
+    console.log('explode')
+    localStorage.removeItem('rockband_list')
+    localStorage.removeItem('rockband_index')
+    a = buildSongList()
+    index = updateIndex(0);
+    goTo(a, index)
+    $('#results').val(convertToJS(a));
+  })
 
   //key press
   $(document).keydown(function(e) {
     switch(e.which) {
       //left
-      case 37: index = addAndGo(a, index, 'Expired')
+      case 37:
+      a = updateList(a, index, 'y', a[index][3] || '')
+      if (index < a.length) index = updateIndex(index + 1)
+      goTo(a, index)
       break;
 
       //up
       case 38:
-        $('#addingArea').toggle();
-        $('addText').focus();
+      if (index > 0) index = updateIndex(index - 1);
+      goTo(a, index)
       break;
 
       //right
-      case 39: index = addAndGo(a, index, 'Y')
+      case 39:
+      a = updateList(a, index, '', a[index][3] || '')
+      if (index < a.length) index = updateIndex(index + 1)
+      goTo(a, index)
       break;
 
       //down
-      case 40: index = addAndGo(a, index, '')
+      case 40:
+      if (index < a.length) index = updateIndex(index + 1)
+      goTo(a, index)
       break;
 
       default: return; // exit this handler for other keys
