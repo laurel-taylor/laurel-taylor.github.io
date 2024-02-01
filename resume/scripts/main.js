868dd475249c8996647359cfc74c3a11afb2acde
Laurel Taylor <laurel.taylor@keap.com>
2024-02-01 14:50:55 -0700

    update resume

--- resume/scripts/main.js
+++ resume/scripts/main.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
   $(document).on('click', '#silly-button', function(e) {
     e.preventDefault();
     $('.silly').show();
+    $('.serious').hide();
     window.scrollTo(0,document.body.scrollHeight)
   })
   $(document).on('click', '#serious-button', function(e) {
     e.preventDefault();
     $('.silly').hide()
+    $('.serious').hide()
   })
 })
